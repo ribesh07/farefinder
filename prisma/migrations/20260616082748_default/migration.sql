dad855804f732ca5d2339889cecb32a7d04ba2f9
ribesh07 <ribeshkumarsah@gmail.com>
2026-06-16 14:13:53 +0545

    schema

---
+++ prisma/migrations/20260616082748_default/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "WebsiteSettings" ALTER COLUMN "supportPhone" SET DEFAULT '447599064917',
+ALTER COLUMN "whatsappNumber" SET DEFAULT '447599064917',
+ALTER COLUMN "officeAddress" SET DEFAULT 'Glassgow, UK';
